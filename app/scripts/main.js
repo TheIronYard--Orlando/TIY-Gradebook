eaea5db01ebf7088d8cd1bae2db0d3d060206bf9
David Rogers <al.the.x@gmail.com>
2015-04-11 22:36:52 -0400

    Seems like a good breaking point: List of "Classes" (Repos) and a "Class" page...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,39 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+(function(){
+  var view = new Vue({
+    el: '.application',
+    data: {
+      page: 'classes',
+      repos: [ ],
+      repo: null,
+    },
+    methods: {
+      isPage: function(name){
+        return this.page === name;
+      },
+      repoByName: function(name){
+        return _.first(this.repos, { name: name });
+      },
+    }
+  });
+
+  var repos = $.getJSON('apis/github/orgs/TheIronYard--Orlando/repos.json')
+    .done(function(data){
+      view.repos = data.filter(function(repo){
+        return repo.name.match(/^(FEE|ROR|iOS)--/);
+      }).reverse();
+    });
+
+  page('/', function(context){
+    view.page = 'classes';
+  });
+
+  page('/class/:repo', function(context){
+    view.page = 'class';
+
+    view.repo = context.params.repo;
+  });
+
+  page({ hashbang: true });
+
+})();
